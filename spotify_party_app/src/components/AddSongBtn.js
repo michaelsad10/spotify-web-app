268d12ed91d6a17dc4ff59cc86e6dd394abfb776
Michael <michaelsad10@gmail.com>
2019-10-30 12:15:07 -0500

    Pushing changes for playlist choice button

--- spotify_party_app/src/components/AddSongBtn.js
+++ spotify_party_app/src/components/AddSongBtn.js
@@ -5,18 +5,26 @@ import axios from 'axios';
 class AddSongBtn extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            choice : 'Playlist'
+        }
+    }
+
+    handleChoice = (choice) => {
+        console.log(choice); 
+        this.setState({choice : choice}); 
     }
     render() {
         var options = [];
-        var playlist = 'Playlist';
         var choice = <Dropdown.Toggle variant="success" id="dropdown-basic">
-            {playlist}
+            {this.state.choice}
         </Dropdown.Toggle>
         var playlists_obj = {};
         for (var x=0; x < this.props.playlists_name.length; x++) {
+            var name = this.props.playlists_name[x];
             playlists_obj[this.props.playlists_name[x]] = this.props.playlists_id[x];
             options.push(
-                <Dropdown.Item key={x}> {this.props.playlists_name[x]} </Dropdown.Item>
+                <Dropdown.Item key={x} onClick={() => this.handleChoice(name)}> {this.props.playlists_name[x]} </Dropdown.Item>
             );
         }
         return (
